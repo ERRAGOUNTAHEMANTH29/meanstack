3e5d4545c12673a1e4f22caaae707a95dc0362ba
ERRAGOUNTAHEMANTH <hemantherragounta29@gmail.com>
2024-05-20 11:10:32 +0530

    updated

---
+++ frontend/javascript/mathfunctions and date functions/md.js
@@ -0,0 +1,27 @@
+let a = 10+10;
+console.log(a);
+let b = 10-10;
+console.log(b);
+let c= 10*10;
+console.log(c);
+let d = 10/10;
+console.log(d);
+let e = 10%10;
+console.log(e);
+
+let sine =Math.sin(Math.PI/2);
+console.log(sine);
+let cosine=Math.cos(0);
+console.log(cosine);
+let tan =Math.tan(Math.PI/4);
+console.log(tan);
+
+let power =Math.pow(2,3);
+console.log(power);
+let sqrt =Math.sqrt(36);console.log(sqrt);
+let log=Math.log(43);console.log(log);
+
+let round =Math.round(0.7);console.log(round);
+let ceil =Math.ceil(10.1);console.log(ceil);
+let floor =Math.floor(10.7);console.log(floor);
+let abs = Math.abs(-13);console.log(abs);
